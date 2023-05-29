33592350b65df05b6ea96763b58946b756bab797
Alina Potapchuk <alinapotapchuk7@gmail.com>
2023-05-29 16:40:19 +0300

    first prepare

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# movies-explorer-api
+# Дипломный проект курса Яндекс.Практикум. Бэкенд проекта movies-explorer
+
+## Запуск проекта
+
+`npm run start` — запускает сервер   
+`npm run dev` — запускает сервер с hot-reload
+`npm lint` - запускает проверку линтера
+
+## Расположение проекта
+
+Ссылка на репо: https://github.com/7Alina77/movies-explorer-api
+Ссылка на IP: 
+IP 
---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const { errors } = require('celebrate');
+const routes = require('./routes/index');
+const { handleErrors } = require('./errors/handleErrors');
+
+const app = express();
+const { PORT = 3000 } = process.env;
+
+mongoose.connect('mongodb://127.0.0.1:27017/bitfilmsdb');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(routes);
+app.use(errors());
+app.use(handleErrors);
+
+app.listen(PORT);
